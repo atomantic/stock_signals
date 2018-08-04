3033028eb2e8af8635d977e002de7a35a8315163
Adam Eivy <atomantic@gmail.com>
2018-08-04 12:28:19 -0700

    /sync endpoint for cleaning results

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const {	each, now } = require('lodash')
 const results = require('../data/results')
 const config = require('../config')
 const request = require('request')
@@ -27,6 +28,31 @@ module.exports = [{
       // set two back so we will increment into it after the current run finishes
       runnerData.currentIndex = i-2
       return h.response(runnerData.currentIndex)
+    }
+  },{
+    method: 'GET',
+    path: '/sync',
+    handler: function(req, h){
+      // clean the results list to only contain the tickers present in the ticker list
+      each(results.results, function(value, key){
+        if(config.tickers.indexOf(key)===-1){
+          // should not exist
+          delete results.results[key]
+        }
+      })
+      // ensure that there is a blank ticker result for any that are missing
+      each(config.tickers, function(key){
+        if(!results.results[key]){
+          results.results.key = {
+            signal: '?',
+            day: {},
+            hours: {},
+            updated: now(),
+            week: {}
+          }
+        }
+      })
+      return h.response(results)
     }
   },{
     method: 'GET',
